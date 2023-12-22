2d68e6b33150bb3a0ec8280173e59487598ecc93
PKPakpoom <pai38985@gmail.com>
2023-12-22 20:37:12 +0700

    make things working

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-import os
+import os, csv
 from dotenv import load_dotenv
 from scrapper import StockChartsScrapper
-from cleaner import to_csv
 
 load_dotenv()
 
-stock_name = "INTC"
-
 urls = {
-    "login": "https://stockcharts.com/login/index.php",
-    "get_stock": "https://stockcharts.com/h-hd/?"
+    "login_url": "https://stockcharts.com/login/index.php",
+    "get_stock_url": "https://stockcharts.com/h-hd/?",
+    "get_stocks_name_url": "https://stockcharts.com/freecharts/catalog/?sl="
 }
 
 xpaths = {
     "username": """//*[@id="form_UserID"]""",
     "password": """//*[@id="form_UserPassword"]""",
     "submit": """//*[@id="loginform"]/fieldset/button""",
-    "data_path": """//*[@id="historical-data-body"]/div/pre"""
+    "stock_data_path": """//*[@id="historical-data-body"]/div/pre""",
+    "stock_names_upper_row": """//*[@id="symcat-links"]""",
+    "stock_names_lower_row": """//*[@id="symcat-links-subsection"]""",
+    "stock_table_head": """//*[@id="symcat-table"]/thead""",
+    "stock_table_body": """//*[@id="symcat-table"]/tbody"""
+}
+
+class_name = {
+    "stock_names_upper_row": "catalog",
+    "stock_names_lower_row": "btn-white"
 }
 
 credential = {
@@ -25,28 +32,71 @@ credential = {
 }
 
 def login(driver: StockChartsScrapper) -> None:
-    driver.go_url(url=urls["login"])
+    driver.go_url(url=urls["login_url"])
     driver.fill_input(xpath=xpaths["username"], value=credential["username"])
     driver.fill_input(xpath=xpaths["password"], value=credential["password"])
     driver.click_btn(xpath=xpaths["submit"])
 
+from selenium.webdriver.common.by import By
+
+def scrape_all_stocks(driver: StockChartsScrapper) -> None:
+    all_upper_url = []
+    all_lower_url = []
+    driver.go_url(urls["get_stocks_name_url"])
+    for ba in driver.get_children_by_class_name(class_name["stock_names_upper_row"]):
+        all_upper_url.append(ba.get_attribute("href"))
+
+    with open("./stocks.txt", "w") as f:
+        for ua in all_upper_url:
+            print("-> getting stock names from {}".format(ua))
+            driver.go_url(ua)
+            for bb in driver.get_children_by_class_name(class_name["stock_names_lower_row"]):
+                all_lower_url.append(bb.get_attribute("href"))
+            
+            with open("./stocks.txt", "w") as f:
+                for ub in all_lower_url:
+                    driver.go_url(ub)
+                    table = driver.driver.find_element(By.XPATH, xpaths["stock_table_body"])
+                    for row in table.find_elements(By.TAG_NAME, "tr"):
+                        tds = row.find_elements(By.TAG_NAME, "td")
+                        f.write(tds[1].text + "\n")
+    print("-> done getting stock names")
+
+def get_stock_data(driver: StockChartsScrapper, stock_name: str):
+    driver.go_url(url=urls["get_stock_url"] + stock_name)
+    data = driver.get_data(xpath=xpaths["stock_data_path"])
+    with open("./datas/{}.txt".format(stock_name), "w") as f:
+        f.write(data)
+    print("-> done getting {}".format(stock_name))
+
+
+def txt_to_csv(from_path: str, to_path: str) -> None:
+    with open(from_path, 'r') as txt_file:
+        next(txt_file)
+        columns = txt_file.readline().split()
+        next(txt_file)
+        with open(to_path, 'w') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(columns)
+            while next(txt_file, "EOF") != "EOF":
+                csv_writer.writerow(txt_file.readline().split())
+
 def main():
     scrape = StockChartsScrapper()
     login(driver=scrape)
-    scrape.go_url(url=urls["get_stock"] + stock_name)
-
-    data = scrape.get_data(xpath=xpaths["data_path"])
 
     if not os.path.exists("./datas"):
         os.makedirs("./datas")
-    
-    with open("./datas/{}.txt".format(stock_name), "w") as f:
-        f.write(data)
-    
-    to_csv(from_path="./datas/{}.txt".format(stock_name), to_path="./datas/{}.csv".format(stock_name))
 
-    os.remove("./datas/{}.txt".format(stock_name))
-    
+    scrape_all_stocks(scrape)
+
+    with open("./stocks.txt", "r") as f:
+        for stock_name in f.readlines():
+            stock_name = stock_name.strip()
+            print("-> getting data from {}".format(stock_name))
+            get_stock_data(driver=scrape, stock_name=stock_name)
+            txt_to_csv(from_path="./datas/{}.txt".format(stock_name), to_path="./datas/{}.csv".format(stock_name))
+            os.remove("./datas/{}.txt".format(stock_name))
 
     scrape.close()
 
--- scrapper.py
+++ scrapper.py
@@ -7,14 +7,21 @@ class StockChartsScrapper():
     def __init__(self) -> None:
         options = Options()
         options.add_argument("--headless")
+        options.add_argument("--log-level=3")
         self.driver = webdriver.Chrome(options=options)
 
     def go_url(self, url: str) -> None:
         self.driver.get(url=url)
 
-    def fill_input(self, xpath: str, value: str) -> None:
+    def fill_input(self, xpath: str, value: str):
         self.driver.find_element(By.XPATH, xpath).send_keys(value)
 
+    def get_children(self, xpath: str) -> list[webdriver.Remote._web_element_cls]:
+        return self.driver.find_elements(By.XPATH, xpath)
+    
+    def get_children_by_class_name(self, class_name: str) -> list[webdriver.Remote._web_element_cls]:
+        return self.driver.find_elements(By.CLASS_NAME, class_name)
+                                                      
     def click_btn(self, xpath: str) -> None:
         self.driver.find_element(By.XPATH, xpath).click()
 
